d26d195c53a4e2a2b45f07db213fa4d1652c3f65
Jason <kangedklan@gmail.com>
2019-06-21 17:33:36 -0400

    created method to connect to db and insert new job entries

--- src/com/github/ewyk/jobapplicationtracker/JobEntry.java
+++ src/com/github/ewyk/jobapplicationtracker/JobEntry.java
@@ -1,13 +1,20 @@
 package com.github.ewyk.jobapplicationtracker;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class JobEntry {
-	private int job_entry_id;
+	private static int job_entry_id;
 	private String company;
 	private String position;
 	private String location;
 	private String industry;
 	private String notes;
 	private Progress progressBar;
+	private Connection connect;
+	private Statement sqlStatement;
 	
 	public JobEntry(int job_entry_id, String company, String position, String location, String industry, String notes) {
 		super();
@@ -41,6 +48,23 @@ public class JobEntry {
 
 	public String getNotes() {
 		return notes;
+	}
+	
+	public void addJobEntry() {
+		try {
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/job_tracker_db", "root", "PontiacFire2");
+			sqlStatement = connect.createStatement();
+			
+			System.out.println("Connected to database");
+			System.out.println(JobEntry.job_entry_id);
+			String insertInto = "INSERT INTO job_entry (company, position, location, industry, notes)";
+			String values = "VALUES ('" + company + "', '" + position + "', '" + location + "', '" + industry + "', '" + notes + "');";
+			
+			sqlStatement.executeUpdate(insertInto + values);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 	
 	
