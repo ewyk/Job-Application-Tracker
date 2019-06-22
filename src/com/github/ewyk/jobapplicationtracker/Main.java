7b5b1671528aaa49e3b79a12770864d37ff03068
Jason <kangedklan@gmail.com>
2019-06-22 16:19:04 -0400

    connected to mysql db

--- src/com/github/ewyk/jobapplicationtracker/Main.java
+++ src/com/github/ewyk/jobapplicationtracker/Main.java
@@ -3,6 +3,10 @@ package com.github.ewyk.jobapplicationtracker;
 public class Main {
 
 	public static void main(String[] args) {
+		JobEntry job1 = new JobEntry("Amazon", "SDE", "Seattle", "tech industry", "");
+		
+		job1.addJobEntry();
+		
 
 	}
 
