bc11ba0801b26bf0700a89b79932ec8661eefae3
Jason <kangedklan@gmail.com>
2019-06-24 19:20:10 -0400

    delete entry and print table methods

--- src/com/github/ewyk/jobapplicationtracker/JobEntry.java
+++ src/com/github/ewyk/jobapplicationtracker/JobEntry.java
@@ -2,6 +2,7 @@ package com.github.ewyk.jobapplicationtracker;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -13,8 +14,6 @@ public class JobEntry {
 	private String industry;
 	private String notes;
 	private Progress progressBar;
-	private Connection connect;
-	private Statement sqlStatement;
 	
 	//MySQL auto increments job_entry_id, so id might not be needed in constructor
 	public JobEntry(String company, String position, String location, String industry, String notes) {
@@ -53,16 +52,83 @@ public class JobEntry {
 	
 	//this method connects to db and creates statements to add values to table
 	public void addJobEntry() {
-		try {
-			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/job_tracker_db", "root", "PontiacFire2");
-			sqlStatement = connect.createStatement();
-			
+		//try-with-resources statement automatically closes declared resources when try block exits
+		try(Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/job_tracker_db", "root", "PontiacFire2");
+			PreparedStatement prepStmt = connect.prepareStatement("INSERT INTO job_entry (company, position, location, industry, notes) VALUES (?, ?, ?, ?, ?)");
+		) {
+						
 			System.out.println("Connected to database");
-			System.out.println(JobEntry.job_entry_id);
-			String insertInto = "INSERT INTO job_entry (company, position, location, industry, notes)";
-			String values = "VALUES ('" + company + "', '" + position + "', '" + location + "', '" + industry + "', '" + notes + "');";
 			
-			sqlStatement.executeUpdate(insertInto + values);
+			prepStmt.setString(1, company);
+			prepStmt.setString(2, position);
+			prepStmt.setString(3, location);
+			prepStmt.setString(4, industry);
+			prepStmt.setString(5, notes);
+			
+//			prepStmt.executeUpdate();
+			int numRows = prepStmt.executeUpdate();
+			System.out.println(numRows + " entry added");
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	//TODO: might have to make separate update methods for each column
+//	public void updateJobEntry() {
+//		try {
+//			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/job_tracker_db", "root", "PontiacFire2");
+//			sqlStatement = connect.createStatement();
+//			
+//			String sql = "UPDATE job_tracker_db " 
+//					+ "SET"
+//		}
+//		catch(Exception e) {
+//			System.out.println(e);
+//		}
+//		finally {
+//			try {
+//				sqlStatement.close();
+//			}
+//			catch (Exception e){
+//				System.out.println(e);
+//			}
+//			try {
+//				connect.close();
+//			}
+//			catch (Exception e){
+//				System.out.println(e);
+//			}
+//		}
+//	}
+	//deletes entries in job entry table
+	public void deleteJobEntry(int job_entry_id) {
+		try(Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/job_tracker_db", "root", "PontiacFire2");
+			PreparedStatement prepStmt = connect.prepareStatement("DELETE FROM job_entry WHERE job_entry_id = ?");
+		) {
+			prepStmt.setInt(1, job_entry_id);
+			
+			int numRows = prepStmt.executeUpdate();
+			System.out.println(numRows + " entry deleted");
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	//prints all entries within the job entry table
+	public void printTable() {
+		try(Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/job_tracker_db", "root", "PontiacFire2");
+			Statement sqlStmt = connect.createStatement();
+			ResultSet results = sqlStmt.executeQuery("SELECT * FROM job_entry");
+		) {
+			while(results.next()) {
+				System.out.println(results.getInt("job_entry_id") + " " 
+						+ results.getString("company") + " "
+						+ results.getString("position") + " "
+						+ results.getString("location") + " "
+						+ results.getString("industry") + " "
+						+ results.getString("notes") + " ");
+			}
 		}
 		catch(Exception e) {
 			System.out.println(e);
